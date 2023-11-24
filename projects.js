6c217eb479821be0fb4b94dce8cb94b16a7b8d6f
Lucas8x <lucasemanuel8x@gmail.com>
2023-11-23 21:49:14 -0300

    fix preview img

--- projects.js
+++ projects.js
@@ -1,6 +1,9 @@
 const repoRootURL =
   'https://github.com/Lucas8x/frontendmentor-challenges/tree/main';
 
+const repoRawContent =
+  'https://raw.githubusercontent.com/Lucas8x/frontendmentor-challenges/main';
+
 const isDev = process.env.NODE_ENV !== 'production';
 
 export const projects = [
@@ -9,7 +12,9 @@ export const projects = [
     techs: ['HTML', 'TailwindCSS'],
     image: isDev
       ? './qr-code-component-main/design/desktop-design.jpg'
-      : repoRootURL.concat('/qr-code-component-main/design/desktop-design.jpg'),
+      : repoRawContent.concat(
+          '/qr-code-component-main/design/desktop-design.jpg',
+        ),
     liveURL: './qr-code-component-main',
     repoURL: repoRootURL.concat('/qr-code-component-main'),
   },
